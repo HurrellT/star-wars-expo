b54caa618c43947e07e02099a10058cc63f29e2f
Tomas Hurrell <hurrelltomas@gmail.com>
2025-07-24 13:09:54 -0300

    chore: installed react native reusables

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
 import { Pressable, Text, View } from "react-native";
 import { Button } from "@hurrellt/ui";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
-      <Button text="Click Me" onClick={() => alert("Button Pressed!")} 
-      // className="bg-blue-900" 
-      />
+      <Button text="Click Me" onClick={() => router.push("/person/11")} />
     </View>
   );
 }
