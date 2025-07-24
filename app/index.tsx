a7f30246acaa4847e5009e612c0b3865d86ce17a
Tomas Hurrell <hurrelltomas@gmail.com>
2025-07-24 12:40:17 -0300

    chore: added cross-platform button

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { Button } from "@hurrellt/ui";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+      <Button text="Click Me" onClick={() => alert("Button Pressed!")} 
+      // className="bg-blue-900" 
+      />
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@hurrellt/ui/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
   presets: [require("nativewind/preset")],
-}
-
+};
